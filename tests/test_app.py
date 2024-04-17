95fb7ccbac89bc0e69cfbf20a2c0b764e87eb905
Gregory Hugaerts <gregory.hugaerts@gmail.com>
2024-04-17 13:41:57 +0200

    add another integration test

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,6 +26,16 @@ def test_app_localhost_with_closed_port(mocker):
     assert f"port 20 on {_LOCALHOST} is closed" in result.stdout
 
 
+def test_app_localhost_with_multiple_port(mocker):
+    mocker.patch("port_scanner.app.is_port_open", return_value=False)
+
+    result = runner.invoke(app, ["--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "21"])
+    assert result.exit_code == 0
+    assert f"{_LOCALHOST} seems to be up" in result.stdout
+    assert f"port 20 on {_LOCALHOST} is closed" in result.stdout
+    assert f"port 21 on {_LOCALHOST} is closed" in result.stdout
+
+
 def test_app_ping_failure(mocker):
     mocker.patch("port_scanner.app.ping", return_value=False)
 
