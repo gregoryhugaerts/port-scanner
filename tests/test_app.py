9a7b8b91e1864c393132fd594f23c00e701848f9
Gregory Hugaerts <gregory.hugaerts@gmail.com>
2024-04-18 09:24:29 +0200

    modify tests to account for new subcommands

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,39 +8,39 @@ runner = CliRunner()
 _LOCALHOST = "127.0.0.1"
 
 
-def test_app_localhost_with_open_port(mocker):
+def test_app_portscan_localhost_with_open_port(mocker):
     mocker.patch("port_scanner.app.is_port_open", return_value=True)
 
-    result = runner.invoke(app, ["--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "20"])
+    result = runner.invoke(app, ["port-scan", "--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "20"])
     assert result.exit_code == 0
     assert "20" in result.stdout
     assert "open" in result.stdout
     assert "closed" not in result.stdout
 
 
-def test_app_localhost_with_closed_port(mocker):
+def test_app_portscan_localhost_with_closed_port(mocker):
     mocker.patch("port_scanner.app.is_port_open", return_value=False)
 
-    result = runner.invoke(app, ["--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "20"])
+    result = runner.invoke(app, ["port-scan", "--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "20"])
     assert result.exit_code == 0
     assert "20" in result.stdout
     assert "closed" in result.stdout
     assert "open" not in result.stdout
 
 
-def test_app_localhost_with_multiple_port(mocker):
+def test_app_portscan_localhost_with_multiple_port(mocker):
     mocker.patch("port_scanner.app.is_port_open", return_value=False)
 
-    result = runner.invoke(app, ["--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "21"])
+    result = runner.invoke(app, ["port-scan", "--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "21"])
     assert result.exit_code == 0
     assert "20" in result.stdout
     assert "21" in result.stdout
 
 
-def test_app_ping_failure(mocker):
+def test_app_portscan_ping_failure(mocker):
     mocker.patch("port_scanner.app.ping", return_value=False)
 
-    result = runner.invoke(app, ["--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "20"])
+    result = runner.invoke(app, ["port-scan", "--host", f"{_LOCALHOST}", "--start-port", "20", "--end-port", "20"])
     assert result.exit_code == 1
 
 
